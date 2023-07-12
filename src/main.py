6985c0ae9fcb1bfc97440a0a8d7fe614147fe4e4
UnsignedArduino <UnsignedArduino@outlook.com>
2023-07-12 11:24:53 -0400

    Start work on settings control

--- src/main.py
+++ src/main.py
@@ -21,6 +21,183 @@ CAMERA_PERIOD = 0.1
 camera = None
 last_frame = bytes()
 
+settings = {
+    "awb_mode": {
+        "selected": "auto",
+        "available": [
+            "off",
+            "auto",
+            "sunlight",
+            "cloudy",
+            "shade",
+            "tungsten",
+            "fluorescent",
+            "incandescent",
+            "flash",
+            "horizon"
+        ]
+    },
+    "brightness": {
+        "min": 0,
+        "max": 100,
+        "value": 50
+    },
+    "contrast": {
+        "min": -100,
+        "max": 100,
+        "value": 0
+    },
+    "effect": {
+        "selected": "none",
+        "available": [
+            "none",
+            "negative",
+            "solarize",
+            "sketch",
+            "denoise",
+            "emboss",
+            "oilpaint",
+            "hatch",
+            "gpen",
+            "pastel",
+            "watercolor",
+            "film",
+            "blur",
+            "saturation",
+            "colorswap",
+            "washedout",
+            "posterise",
+            "colorpoint",
+            "colorbalance",
+            "cartoon",
+            "deinterlace1",
+            "deinterlace2"
+        ]
+    },
+    "iso": {
+        "selected": 0,
+        "available": [
+            0,
+            100,
+            200,
+            320,
+            400,
+            500,
+            640,
+            800
+        ]
+    },
+    "resolution": {
+        "selected": (720, 480),
+        "available": [
+            "128x96",
+            "160x120",
+            "160x144",
+            "176x144",
+            "180x132",
+            "180x135",
+            "192x144",
+            "234x60",
+            "256x192",
+            "320x200",
+            "320x240",
+            "320x288",
+            "320x400",
+            "352x288",
+            "352x240",
+            "384x256",
+            "384x288",
+            "392x72",
+            "400x300",
+            "460x55",
+            "480x320",
+            "468x32",
+            "468x60",
+            "512x342",
+            "512x384",
+            "544x372",
+            "640x350",
+            "640x480",
+            "640x576",
+            "704x576",
+            "720x350",
+            "720x400",
+            "720x480",
+            "720x483",
+            "720x484",
+            "720x486",
+            "720x540",
+            "720x576",
+            "729x348",
+            "768x576",
+            "800x600",
+            "832x624",
+            "856x480",
+            "896x600",
+            "960x720",
+            "1024x576",
+            "1024x768",
+            "1080x720",
+            "1152x768",
+            "1152x864",
+            "1152x870",
+            "1152x900",
+            "1280x720",
+            "1280x800",
+            "1280x854",
+            "1280x960",
+            "1280x992",
+            "1280x1024",
+            "1360x766",
+            "1365x768",
+            "1366x768",
+            "1365x1024",
+            "1400x788",
+            "1400x1050",
+            "1440x900",
+            "1520x856",
+            "1536x1536",
+            "1600x900",
+            "1600x1024",
+            "1600x1200",
+            "1792x1120",
+            "1792x1344",
+            "1824x1128",
+            "1824x1368",
+            "1856x1392",
+            "1920x1080",
+            "1920x1200",
+            "1920x1440",
+            "2000x1280",
+            "2048x1152",
+            "2048x1536",
+            "2048x2048",
+            "2500x1340",
+            "2560x1600",
+            "3072x2252",
+            "3600x2400"
+        ]
+    },
+    "saturation": {
+        "min": -100,
+        "max": 100,
+        "value": 0
+    },
+    "servos": {
+        "enable": True,
+        "pan": {
+            "min": 0,
+            "max": 180,
+            "value": 90
+        },
+        "tilt": {
+            "min": 0,
+            "max": 60,
+            "value": 30
+        }
+    }
+}
+
 
 async def capture_frames():
     global last_frame
@@ -66,7 +243,35 @@ async def websocket_stream(ws: WebSocket):
             await ws.send_bytes(last_frame)
             await aio.sleep(CAMERA_PERIOD)
     except WebSocketDisconnect:
-        logger.info("Client disconnect from websocket stream")
+        logger.info("Client disconnected from websocket stream")
+
+
+control_clients: list[WebSocket] = []
+
+
+@app.websocket("/control")
+async def websocket_control(ws: WebSocket):
+    await ws.accept()
+    control_clients.append(ws)
+    logger.info("Client connect to websocket control")
+    try:
+        while True:
+            for client in control_clients:
+                await client.send_json({
+                    "type": "settings",
+                    "settings": settings
+                })
+            new_settings = await ws.receive_json()
+            logger.debug("Received new settings to set")
+            await ws.send_json({
+                "type": "result",
+                "result": True,
+            })
+            await aio.sleep(CAMERA_PERIOD)
+    except WebSocketDisconnect:
+        logger.info("Client disconnected from websocket control")
+    finally:
+        control_clients.remove(ws)
 
 
 if __name__ == "__main__":
